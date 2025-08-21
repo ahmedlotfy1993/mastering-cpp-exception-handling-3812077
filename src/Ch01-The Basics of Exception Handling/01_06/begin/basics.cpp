6fefec2543823cc9172c96c513ba352c9d9392e5
Ahmed Lotfy <ahmedlotfy271993@gmail.com>
2025-08-21 12:04:43 +0000

    Complete 1st exercise

--- src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
+++ src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <exception>
 using namespace std;
 
 const string readConfigFile(const string& filename) 
 {
     ifstream file(filename);
-
+    if(!file.good())throw std::runtime_error {"File couldn't be opened"};
     string line;
     string text;
     while (getline(file, line)) 
@@ -20,10 +20,14 @@ const string readConfigFile(const string& filename)
 }
 
 int main() 
-{    
-    cout << "Reading configuration:" << endl;
-    string text = readConfigFile("configuration.txt");
-    cout << text;
+{    try{
+        cout << "Reading configuration:" << endl;
+        string text = readConfigFile("configuration.txt");
+        cout << text;
+    }catch(const std::exception& ex){
+        cerr << ex.what() << endl;
+        return -1;
+    }
     
     return 0;
 }
